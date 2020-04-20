2a8d3bdbf7e7be729e74cd4467bbf18dfbb5eaf1
Sean Wilson <SeanWilson@seans-imac.mynet>
2020-04-21 00:35:09 +0100

    Update consingment proto location

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"sync"
 
 	// Import the generated protobuf code
-	pb "github.com/<YourUserName>/shippy-service-consignment/proto/consignment"
+	pb "github.com/seandouglaswilson/go-service/proto/consignment"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
